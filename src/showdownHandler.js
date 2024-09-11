ca93742beebb418a8ff10826829706943b2e45e8
mmccorm1ck <mmccorm1ck@outlook.com>
2024-09-11 22:12:35 +0100

    Began implementing showdown login function

--- src/showdownHandler.js
+++ src/showdownHandler.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+const ws = require("ws");
+const axios = require("axios");
+
 const joinGame = (url) => { // Placeholder function for joining active game
   return 'glhf!';
 };
@@ -5,6 +9,12 @@ const joinGame = (url) => { // Placeholder function for joining active game
 const watchReplay = (url) => { // Placeholder function for watching replays
   return 'ggwp!';
 };
+
+async login()
+{
+  const username = process.env.USER;
+  const password = process.env.PASS;
+}
 
 const handler = { // Functions to export
   joinGame,
