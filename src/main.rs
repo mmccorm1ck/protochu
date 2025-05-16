06324bc71863e23ef1597a52ab7b38cc3363dceb
mmccorm1ck <mmccorm1ck@outlook.com>
2025-05-16 14:52:58 +0100

    Implemented basic message reply

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Beep boop");
+use dotenv::dotenv;
+use std::env;
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::prelude::*;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "Beep" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Boop!").await {
+                println!("Error sending message: {why:?}");
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let token = env::var("DISCORD_API_TOKEN").expect("Expected a token in the environment");
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client =
+        Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
+
+    println!("Protochu, I choose you!");
+    if let Err(why) = client.start().await {
+        println!("Client error: {why:?}");
+    }
 }
